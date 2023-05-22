0de7c1fec9cf74d460cf836755240f3340b7df37
Bruno Menna <brunocesarmenna@gmail.com>
2023-05-22 00:48:38 -0300

    added sidebar for brand filtering

--- src/components/PhoneCatalog/PhoneCatalog.js
+++ src/components/PhoneCatalog/PhoneCatalog.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import PhoneItem from "../PhoneItem/PhoneItem";
 
-const PhoneCatalog = ({ phones }) => {
-  const phonesMapped = phones.map((p) => (
+const PhoneCatalog = ({ phones, brandFilterSelected }) => {
+  const phonesFiltered = phones
+    .filter((p) => p.brand === brandFilterSelected)
+    .map((p) => (
+      <PhoneItem id={p.id} brand={p.brand} model={p.model} price={p.price} />
+    ));
+
+  const noFilteredPhones = phones.map((p) => (
     <PhoneItem id={p.id} brand={p.brand} model={p.model} price={p.price} />
   ));
-  return phonesMapped;
+  return <>{phonesFiltered.length === 0 ? noFilteredPhones : phonesFiltered}</>;
 };
 
 export default PhoneCatalog;
