b26fb224291fbb0e925639af020c746cca18f275
Bruno Menna <brunocesarmenna@gmail.com>
2023-05-23 18:32:56 -0300

    Revert "Merge pull request #10 from BrunoCMenna/gui6"
    This reverts commit 2c01db903904dfd1ca387e23e5c286b1d86dc0a6, reversing
    changes made to a29c0a63beeb77ee61292c203e4b4178d30814a9.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,10 +2,9 @@ import React from "react";
 import { FaShoppingCart } from 'react-icons/fa';
 import { HiChevronDoubleRight } from "react-icons/hi"
 import { useNavigate } from "react-router-dom";
-import Shop from "../Shop/Shop";
 import "./NavBar.css";
 
-const NavBar = ({ id, brand, model, price }) => {
+const NavBar = () => {
   const navigation = useNavigate();
 
   const goLogin = () => {
@@ -17,21 +16,7 @@ const NavBar = ({ id, brand, model, price }) => {
       <nav className="navbar px-4">
         <div className="flex-grow-1 text-white">Empresa</div>
         <div className="CRI">
-
-          <div class="dropdown">
-            <span><button type="button"> <h2> <FaShoppingCart /> </h2></button></span>
-            <div class="dropdown-content">
-              <ul class="px-0">
-                <h3>CARRITO</h3>
-                <ul className="items px-0">
-                  <p>
-                    <Shop brand={brand} model={model} price={price} />
-                  </p>
-                </ul>
-              </ul>
-            </div>
-          </div>
-
+          <button className="cart-icon"><h2 className="px-1"><FaShoppingCart /></h2>Carrito</button>
           <button onClick={goLogin} type="button">Registrarse</button>
           <button onClick={goLogin} type="button"><HiChevronDoubleRight />Iniciar sesión</button>
         </div>
